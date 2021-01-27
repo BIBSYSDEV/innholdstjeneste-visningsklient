027f9c5c8a868f125e4e2e80ba58431d175198b0
Eirik Eide Saur <eisa@unit.no>
2021-01-27 14:47:12 +0100

    Added content to display data

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,95 @@ import "./App.css";
 import styled from "styled-components";
 import Header from "./components/Header";
 import CollapsedBox from "./components/CollapsedBox";
+import pesljegere from "./resources/pelsjegere.jpg";
 
 const OuterContainer = styled.div`
-  background-color: #fafafa;
   position: absolute;
-  bottom: 0;
-  top: 0;
-  right: 0;
   left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  min-height: 100vh;
+`;
+
+const TitleLabel = styled.div`
+  font-family: Barlow, sans-serif;
+  font-size: 35px;
+  font-weight: Bold;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const AuthorLabel = styled.span`
+  display: inline-block;
+  font-family: Barlow, sans-serif;
+  font-size: 25px;
+  margin-left: 1rem;
+  padding-right: 1rem;
+  border-right: 1px solid rgba(0, 0, 0, 0.3);
+`;
+
+const ISBNLabel = styled.span`
+  display: inline-block;
+  font-family: Barlow, sans-serif;
+  font-size: 25px;
+  margin-left: 1rem;
+`;
+
+const ImageContainer = styled.img`
+  position: absolute;
+  right: 3rem;
+  display: block;
+  max-height: 40rem;
+  max-width: 40rem;
+  min-height: 3rem;
+  min-width: 3rem;
 `;
 
 const App: FC = () => {
+  let shortSummary;
+  let longSummary;
+  let tableOfContents;
+  shortSummary = "That sweet sweet content";
+  longSummary =
+    "This is a long version containing information about that same sweet sweet content. It is however important to difrerentiate the two. ";
+  tableOfContents = [
+    "The opening",
+    "Second breakfast",
+    "Three's a party",
+    "May the fourth be with you",
+    "Cinco de mayo",
+  ];
   return (
     <OuterContainer>
       <Header></Header>
-      <h1>Innholdstjeneste</h1>
-      <CollapsedBox
-        name="Beskrivelse fra forlaget (kort)"
-        content="That sweet sweet content sdhasjkdhasjkldhkadshfkah adfjakldfja aldfkjaldfkjdsalkf jdlkfjadfk asdlkfjadlkfjadfk ajdflkahjdf kajdfl kahjdf ljk"
-        open={true}
-      ></CollapsedBox>
-      <br></br>
-      <CollapsedBox
-        name="Beskrivelse fra forlaget (Lang)"
-        content="That sweet sweet content sdhasjkdhasjkldhkadshfkah adfjakldfja aldfkjaldfkjdsalkf jdlkfjadfk asdlkfjadlkfjadfk ajdflkahjdf kajdfl kahjdf ljk"
-        open={false}
-      ></CollapsedBox>
+      <TitleLabel>The Book of Books</TitleLabel>{" "}
+      <ImageContainer src={pesljegere}></ImageContainer>
+      <AuthorLabel>Helge Ingstad</AuthorLabel>
+      <ISBNLabel>ISBN: 9788205377547</ISBNLabel>
+      {shortSummary && (
+        <CollapsedBox
+          name="Beskrivelse fra forlaget (kort)"
+          summary={shortSummary}
+          open={true}
+        ></CollapsedBox>
+      )}
+      {longSummary && (
+        <CollapsedBox
+          name="Beskrivelse fra forlaget (lang)"
+          summary={longSummary}
+          open={!shortSummary}
+        ></CollapsedBox>
+      )}
+      {tableOfContents && (
+        <CollapsedBox
+          name="Innholdfortegnelse"
+          contents={tableOfContents}
+          open={!shortSummary && !longSummary}
+        ></CollapsedBox>
+      )}
     </OuterContainer>
   );
 };
--- src/components/CollapsedBox.tsx
+++ src/components/CollapsedBox.tsx
@@ -3,38 +3,64 @@ import styled from "styled-components";
 
 interface CollapsedBoxProps {
   name: String;
-  content: String;
+  summary?: String;
+  contents?: String[];
   open: boolean;
 }
 
 const CollapseContainer = styled.div`
-  background-color: Grey;
+  background-color: rgba(0, 0, 0, 0.3);
   border-radius: 5px;
   margin-left: 1rem;
   margin-right: 1rem;
   padding-left: 1rem;
   padding-right: 1rem;
   padding-bottom: 0.5rem;
-  width: 40%;
+  margin-top: 1rem;
+  width: 60%;
 `;
 
 const CollapseHandle = styled.span`
   background-color: white;
-  font: Barlow, sans-serif;
+  color: rgb(40, 75, 99);
+  font-family: Barlow, sans-serif;
+  font-size: 23px;
+  font-weight: Bold;
   display: inline-block;
   border-radius: 5px;
   margin-top: 0.5rem;
+  padding-top: 0.2rem;
+  padding-bottom: 0.2rem;
   width: 100%;
   cursor: pointer;
 `;
 
-const CollapsedContent = styled.p`
+const CollapsedSummary = styled.p`
   background-color: white;
+  font-family: Barlow, sans-serif;
+  font-size: 20px;
+  border-radius: 5px;
   padding: 1rem;
-  pointer-events: none;
+  margin-top: 0.5rem;
+  margin-bottom: 0;
 `;
 
-const CollapsedBox: FC<CollapsedBoxProps> = ({ name, content, open }) => {
+const CollapsedContents = styled.ul`
+  background-color: white;
+  font-family: Barlow, sans-serif;
+  font-size: 20px;
+  border-radius: 5px;
+  padding: 1rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0;
+`;
+
+const CollapsedBox: FC<CollapsedBoxProps> = ({
+  name,
+  summary,
+  contents,
+  open,
+}) => {
   const [showKortInfo, setShowKortInfo] = useState(open);
 
   const handleKortInfoClick = () => {
@@ -46,7 +72,16 @@ const CollapsedBox: FC<CollapsedBoxProps> = ({ name, content, open }) => {
       <CollapseHandle onClick={handleKortInfoClick}>
         {showKortInfo ? "▼" : "▶"} {name}
       </CollapseHandle>
-      {showKortInfo && <CollapsedContent>{content}</CollapsedContent>}
+      {showKortInfo && summary && (
+        <CollapsedSummary>{summary}</CollapsedSummary>
+      )}
+      {showKortInfo && contents && (
+        <CollapsedContents>
+          {contents.map((value, index) => {
+            return <p key={index}>{value}</p>;
+          })}
+        </CollapsedContents>
+      )}
     </CollapseContainer>
   );
 };
