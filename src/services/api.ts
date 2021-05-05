c86d86ef8f14af59ba09e3d502ef9ce943d9030c
Ingrid Hjulstad <inhj@unit.no>
2021-05-05 14:47:52 +0200

    SMILE-726 Dobbelt linjeskift for <p> og en litt annen måte å erstatte noen tags med linjeskift på

--- src/services/api.ts
+++ src/services/api.ts
@@ -17,9 +17,9 @@ export const getInnholdsinformasjon = async (isbn: string): Promise<Innholdsform
     innholdResponse.image_original,
     innholdResponse.image_large
   );
-  innholdsinformasjon.description_short = splitOnTags(innholdResponse.description_short).map(removeAllTags);
-  innholdsinformasjon.description_long = splitOnTags(innholdResponse.description_long).map(removeAllTags);
-  innholdsinformasjon.table_of_contents = splitOnTags(innholdResponse.table_of_contents).map(removeAllTags);
+  innholdsinformasjon.description_short = splitOnSomeTags(innholdResponse.description_short).map(removeAllTags);
+  innholdsinformasjon.description_long = splitOnSomeTags(innholdResponse.description_long).map(removeAllTags);
+  innholdsinformasjon.table_of_contents = splitOnSomeTags(innholdResponse.table_of_contents).map(removeAllTags);
 
   return innholdsinformasjon;
 };
@@ -36,18 +36,17 @@ const removeAllTags = (value: string) => {
   return value.replace(/(<([^>]+)>)/gi, '');
 };
 
-const splitOnTags = (field: string) => {
+const splitOnSomeTags = (field: string) => {
   if (field) {
-    return field
-      .split('<br>')
-      .join('###')
-      .split('<br />')
-      .join('###')
-      .split('<p>')
-      .join('###')
-      .split('<li>')
-      .join('###')
-      .split('###');
+    const replacedSomeTagsWithNewLine = field
+      .replaceAll('\\n', '')
+      .replaceAll('<br>', '\\n')
+      .replaceAll('<br />', '\\n')
+      .replaceAll('<p>', '\\n\\n')
+      .replaceAll('<li>', '\\n')
+      .replaceAll('<br>', '\\n');
+
+    return replacedSomeTagsWithNewLine.split('\\n');
   }
   return [];
 };
