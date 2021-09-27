ea8e1443c21b38eb0496d808ec756884dc5ff33f
pcb <pcb@unit.no>
2021-09-27 12:54:43 +0200

    SMILE-1004 - bugfix

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK === 'false';
+export const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK === 'true';
 export const API_URL = process.env.REACT_APP_INNHOLDSTJENESTE_API_URL;
